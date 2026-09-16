46fc7fdb2b91b87821c3b005c834fb5741f15139
Ryvn Technologies <github-actions[bot]@users.noreply.github.com>
2026-09-16 21:12:57 +0000

    chore: update version to v1.264.0

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ GITHUB_REPO="ryvn-technologies/ryvn-cli-release"
 BINARY_NAME="ryvn"
 INSTALL_DIR="${INSTALL_DIR:-$HOME/.ryvn/bin}"
 TMP_DIR=""
-VERSION="v1.263.0" # This will be automatically updated by GitHub workflow
+VERSION="v1.264.0" # This will be automatically updated by GitHub workflow
 if [[ -n "${RYVN_CLI_VERSION:-}" ]]; then
     # Accept a bare "1.2.3"; release tags carry a leading "v".
     VERSION=v${RYVN_CLI_VERSION#v}
